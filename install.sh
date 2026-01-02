c60ae4ff9f6a14cda01402ddfa196b98e1049b27
EthanDGee <workethangee@gmail.com>
2026-01-01 20:57:18 -0700

    Set up the zellij-favs plug for better management

--- install.sh
+++ install.sh
@@ -6,3 +6,7 @@ curl -L https://github.com/karimould/zellij-forgot/releases/download/0.4.2/zelli
 
 # install room - a simple tab switching floating window
 curl -L "https://github.com/rvcas/room/releases/latest/download/room.wasm" -o ~/.config/zellij/plugins/room.wasm
+
+# install (and set up) zellij-favs - way of easily saving favorite sessions, and flushing unwanted ones
+curl -L https://github.com/JoseMM2002/zellij-favs/releases/download/v1.0.1/zellij-favs.wasm -o ~/.config/zellij/plugins/zellij-favs.wasm
+mkdir -p ~/.cache/zellij-favs/
